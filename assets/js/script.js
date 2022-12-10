ace22c6361a7df4632755446e038d5120ac9e803
Don Carderms <donphilocardermssouffrant@gmail.com>
2022-12-10 18:32:09 -0300

    my first commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,8 +29,8 @@ add.addEventListener('click',  (event)=> {
                 btnEdit.setAttribute('class', "btn-edit");
         
                 paragrafo.innerHTML = `${texto}`;
-                btnDelete.innerHTML = "excluir";
-                btnEdit.innerHTML = "Editar";
+                btnDelete.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM7 9a1 1 0 000 2h6a1 1 0 100-2H7z" clip-rule="evenodd"></path></svg>';
+                btnEdit.innerHTML = '<img src="/assets/images/edit-pencil-svgrepo-com.svg" alt="">';
               
                 paragrafo.setAttribute('class', "todo-list-p")
                 paragrafo.setAttribute('id', index);
